04ce892aa80dae5ecae70bb2f73db55daa40f9d6
Andrii <107649409+Andriy-Kond@users.noreply.github.com>
2023-03-28 11:14:26 +0300

    06

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,60 +1,60 @@
-// // todo: Рефакторінг затримок за допомогою useMemo
-// // import { Component } from 'react';
-// import { Section } from './Section/Section';
-// import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
-// import { Statistics } from './Statistics/Statistics';
-// import { Notification } from './Notification/Notification';
+// todo: Рефакторінг затримок за допомогою useMemo
+// import { Component } from 'react';
+import { Section } from './Section/Section';
+import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
+import { Statistics } from './Statistics/Statistics';
+import { Notification } from './Notification/Notification';
 
-// import { useState, useReducer } from 'react';
+import { useState, useReducer } from 'react';
 
-// // refactoring to hooks:
-// export const App = () => {
-//   const [feedbackObj, setFeedbackObj] = useState({
-//     good: 0,
-//     neutral: 0,
-//     bad: 0,
-//   });
-//   const { good, neutral, bad } = feedbackObj;
+// refactoring to hooks:
+export const App = () => {
+  const [feedbackObj, setFeedbackObj] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
+  const { good, neutral, bad } = feedbackObj;
 
-//   const handleClick = currentBtnName => {
-//     setFeedbackObj({
-//       ...feedbackObj,
-//       [currentBtnName]: feedbackObj[currentBtnName] + 1,
-//     });
-//   };
+  const handleClick = currentBtnName => {
+    setFeedbackObj({
+      ...feedbackObj,
+      [currentBtnName]: feedbackObj[currentBtnName] + 1,
+    });
+  };
 
-//   const countTotalFeedback = () => {
-//     return good + neutral + bad;
-//   };
+  const countTotalFeedback = () => {
+    return good + neutral + bad;
+  };
 
-//   const countPositiveFeedbackPercentage = total => {
-//     return total && Math.round((100 / total) * good);
-//   };
+  const countPositiveFeedbackPercentage = total => {
+    return total && Math.round((100 / total) * good);
+  };
 
-//   return (
-//     <div>
-//       <Section title="Please leave feedback">
-//         <FeedbackOptions
-//           options={Object.keys(feedbackObj)}
-//           onLeaveFeedback={handleClick}
-//         ></FeedbackOptions>
-//       </Section>
-//       {good || neutral || bad ? (
-//         <Section title="Statistics">
-//           <Statistics
-//             good={good}
-//             neutral={neutral}
-//             bad={bad}
-//             total={countTotalFeedback}
-//             positivePercentage={countPositiveFeedbackPercentage}
-//           ></Statistics>
-//         </Section>
-//       ) : (
-//         <Notification message="There is no feedback"></Notification>
-//       )}
-//     </div>
-//   );
-// };
+  return (
+    <div>
+      <Section title="Please leave feedback">
+        <FeedbackOptions
+          options={Object.keys(feedbackObj)}
+          onLeaveFeedback={handleClick}
+        ></FeedbackOptions>
+      </Section>
+      {good || neutral || bad ? (
+        <Section title="Statistics">
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={countTotalFeedback}
+            positivePercentage={countPositiveFeedbackPercentage}
+          ></Statistics>
+        </Section>
+      ) : (
+        <Notification message="There is no feedback"></Notification>
+      )}
+    </div>
+  );
+};
 
 // export class App extends Component {
 //   state = {
